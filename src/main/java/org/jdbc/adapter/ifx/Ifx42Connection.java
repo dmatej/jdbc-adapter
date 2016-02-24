50257fbd33479e36b714fa12ab58a0aad1ea09f5
David Matejcek <david.matejcek@i.cz>
2016-02-24 15:01:07 +0100

    Ifx42Connectio - implemented networktimeout (throws SqlFeatureNotSupported)

--- src/main/java/org/jdbc/adapter/ifx/Ifx42Connection.java
+++ src/main/java/org/jdbc/adapter/ifx/Ifx42Connection.java
@@ -1,17 +1,22 @@
 package org.jdbc.adapter.ifx;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.sql.Statement;
+import java.util.concurrent.Executor;
 
 import org.jdbc.adapter.iface.StatementWrapper;
 import org.jdbc.adapter.jdbc42.Jdbc42Connection;
+
+import com.informix.jdbc.IfmxConnection;
 
 /**
  * @author David Matějček
  */
 public class Ifx42Connection extends Jdbc42Connection {
 
-  public Ifx42Connection(Connection connection) {
+  public Ifx42Connection(final Connection connection) {
     super(connection);
   }
 
@@ -26,5 +31,27 @@ public class Ifx42Connection extends Jdbc42Connection {
       return null;
     }
     return new Ifx42Statement(statement, this);
+  }
+
+
+  /**
+   * @return the wrapped connection;
+   */
+  @Override
+  protected final IfmxConnection getConnection() {
+    return IfmxConnection.class.cast(getConnection());
+  }
+
+
+  @Override
+  public void setNetworkTimeout(final Executor executor, final int milliseconds) throws SQLException {
+    throw new SQLFeatureNotSupportedException(
+        "This driver does not support the setNetworkTimeout(Executor executor, int milliseconds)");
+  }
+
+
+  @Override
+  public int getNetworkTimeout() throws SQLException {
+    throw new SQLFeatureNotSupportedException("This driver does not support the getNetworkTimeout()");
   }
 }
