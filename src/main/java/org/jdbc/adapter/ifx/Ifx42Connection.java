1f9dc38003b214b3e34097aa4dfc78219bbd11d1
David Matejcek <david.matejcek@i.cz>
2016-03-17 09:13:20 +0100

    Network timeout is not supported on IfmxConnection
    - but throwing an exception causes stacktraces in Glassfish connection holders.
    - this solution should not break anything:
      - setter logs a warning that the call is ignored
      - getter returns 0 (no timeout set)
    - another solution would be to implement the functionality.

--- src/main/java/org/jdbc/adapter/ifx/Ifx42Connection.java
+++ src/main/java/org/jdbc/adapter/ifx/Ifx42Connection.java
@@ -2,9 +2,9 @@ package org.jdbc.adapter.ifx;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
 import java.sql.Statement;
 import java.util.concurrent.Executor;
+import java.util.logging.Logger;
 
 import org.jdbc.adapter.iface.StatementWrapper;
 import org.jdbc.adapter.jdbc42.Jdbc42Connection;
@@ -15,6 +15,7 @@ import com.informix.jdbc.IfmxConnection;
  * @author David Matějček
  */
 public class Ifx42Connection extends Jdbc42Connection {
+  private static final Logger LOG = Logger.getLogger(Ifx42Connection.class.getName());
 
   public Ifx42Connection(final Connection connection) {
     super(connection);
@@ -43,15 +44,21 @@ public class Ifx42Connection extends Jdbc42Connection {
   }
 
 
+  /**
+   * IFXJDBC does not support this method. This wrapper ignores it.
+   */
   @Override
   public void setNetworkTimeout(final Executor executor, final int milliseconds) throws SQLException {
-    throw new SQLFeatureNotSupportedException(
-        "This driver does not support the setNetworkTimeout(Executor executor, int milliseconds)");
+    LOG.warning("This driver does not support the setNetworkTimeout(Executor executor, int milliseconds)."
+        + " Method call was ignored.");
   }
 
 
+  /**
+   * IFXJDBC does not support this method. This wrapper always returns 0 (no timeout).
+   */
   @Override
   public int getNetworkTimeout() throws SQLException {
-    throw new SQLFeatureNotSupportedException("This driver does not support the getNetworkTimeout()");
+    return 0;
   }
 }
